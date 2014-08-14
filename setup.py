77b32d0543ac8096502493ba6793d9331b06ed34
Jan Pipek <jan.pipek@gmail.com>
2014-08-14 14:36:52 +0200

    Set version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydiq',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     license='MIT',
     description='Simple open-source DICOM browser/viewer in Python and Qt4.',
