adb1ea90570b97a4cc22d9b6f27778953ecf1322
Jan Pipek <jan.pipek@gmail.com>
2017-02-24 20:11:55 +0100

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydiq',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     license='MIT',
     description='Simple open-source DICOM browser/viewer in Python and Qt4.',
